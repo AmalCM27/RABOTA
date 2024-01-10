c48b9535155e3adfff131f1e3cb8587a9de27ae2
azamatovamal5@gmail.com <elaman@outlook.com>
2024-01-10 13:37:12 +0600

    commit

--- preolader.js
+++ preolader.js
@@ -1,3 +1,4 @@
 window.onload = function () {
   setTimeout(function () {
     document.getElementById("preloader_malc").style.display = "none";
+    document.getElementById("preloader_malc").style.display = "none";
